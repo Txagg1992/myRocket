aa47adb6c323bffe24ab585a0462d7181a3dd9c6
Enoch Phetteplace <curiousaps@gmail.com>
2024-03-18 17:16:00 -0500

    VM

---
+++ app/src/main/java/com/curiousapps/myrocket/presentation/company_listing/CompanyListingsViewModel.kt
@@ -0,0 +1,69 @@
+package com.curiousapps.myrocket.presentation.company_listing
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.toLowerCase
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.curiousapps.myrocket.domain.repository.StockRepository
+import com.curiousapps.myrocket.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CompanyListingsViewModel @Inject constructor(
+    private val repository: StockRepository
+): ViewModel() {
+
+    private var state by mutableStateOf(CompanyListingsState())
+    private var searchJob: Job? = null
+
+    fun onEvent(event: CompanyListingsEvent){
+        when(event){
+            is CompanyListingsEvent.OnSearchQueryChange -> {
+                state = state.copy(
+                    searchQuery = event.query
+                )
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500L)
+                    getCompanyListings()
+                }
+            }
+            CompanyListingsEvent.Refresh -> {
+                getCompanyListings(fetchFromRemote = true)
+            }
+        }
+    }
+
+    private fun getCompanyListings(
+        query: String = state.searchQuery.toLowerCase(),
+        fetchFromRemote: Boolean = false
+    ){
+        viewModelScope.launch {
+            repository
+                .getCompanyListings(fetchFromRemote, query)
+                .collect(){result ->
+                    when(result){
+                        is Resource.Error -> Unit
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                        is Resource.Success -> {
+                            result.data?.let { listings ->
+                                state = state.copy(
+                                    companies = listings
+                                )
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+}
