50bbf8e53982812eedcfa7c8aaa94700dbabdb65
Enoch Phetteplace <curiousaps@gmail.com>
2024-03-19 10:39:40 -0500

    Compose first screen

---
+++ app/src/main/java/com/curiousapps/myrocket/presentation/company_listing/CompanyItem.kt
@@ -0,0 +1,69 @@
+package com.curiousapps.myrocket.presentation.company_listing
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.curiousapps.myrocket.domain.model.CompanyListing
+
+
+@Composable
+fun CompanyItem(
+    company: CompanyListing,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1F)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = company.exchange,
+                    modifier = Modifier.weight(1f),
+                    color = MaterialTheme.colorScheme.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold
+                    )
+
+                Spacer(
+                    modifier = Modifier.width(4.dp)
+                )
+
+                Text(
+                    text = company.exchange,
+                    fontWeight = FontWeight.Light,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "(${company.symbol})",
+                fontStyle = FontStyle.Italic,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/curiousapps/myrocket/presentation/company_listing/CompanyListingScreen.kt
@@ -0,0 +1,78 @@
+package com.curiousapps.myrocket.presentation.company_listing
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.DialogNavigator
+import androidx.paging.compose.items
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(start = true)
+fun CompanyListingScreen(
+    navigator: DialogNavigator,
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+) {
+    val swipeRefreshState = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.isRefreshing
+    )
+    val state = viewModel.state
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingsEvent.OnSearchQueryChange(it)
+                )
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            singleLine = true,
+            placeholder = {
+                Text(text = "Search...")
+            }
+        )
+        SwipeRefresh(
+            state = swipeRefreshState,
+            onRefresh =
+            { viewModel.onEvent(CompanyListingsEvent.Refresh) }
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(state.companies.size) { i ->
+                    val company = state.companies[i]
+                    CompanyItem(
+                        company = company,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                //TODO: Navigate to detail screen
+                            }
+                            .padding(16.dp)
+                        )
+                    if (i < state.companies.size){
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                    }
+                }
+            }
+        }
+    }
+}
