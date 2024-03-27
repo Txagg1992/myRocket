6ccdb351dbd090552baf1ad862e865bfa42c7675
Enoch Phetteplace <curiousaps@gmail.com>
2024-03-26 23:09:32 -0500

    App bar added to first screen

--- app/src/main/java/com/curiousapps/myrocket/presentation/company_listing/CompanyListingScreen.kt
+++ app/src/main/java/com/curiousapps/myrocket/presentation/company_listing/CompanyListingScreen.kt
@@ -1,14 +1,21 @@
 package com.curiousapps.myrocket.presentation.company_listing
 
+import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,6 +27,7 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph(start = true)
 @Composable
 @Destination
@@ -32,48 +40,64 @@ fun CompanyListingScreen(
     )
     val state = viewModel.state
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        OutlinedTextField(
-            value = state.searchQuery,
-            onValueChange = {
-                viewModel.onEvent(
-                    CompanyListingsEvent.OnSearchQueryChange(it)
-                )
-            },
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Stocks")
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    ) {values ->
+        Column(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            maxLines = 1,
-            singleLine = true,
-            placeholder = {
-                Text(text = "Search...")
-            }
-        )
-        SwipeRefresh(
-            state = swipeRefreshState,
-            onRefresh =
-            { viewModel.onEvent(CompanyListingsEvent.Refresh) }
+                .fillMaxSize()
+                .padding(values)
         ) {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
+            OutlinedTextField(
+                value = state.searchQuery,
+                onValueChange = {
+                    viewModel.onEvent(
+                        CompanyListingsEvent.OnSearchQueryChange(it)
+                    )
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                maxLines = 1,
+                singleLine = true,
+                placeholder = {
+                    Text(text = "Search...")
+                }
+            )
+
+            SwipeRefresh(
+                state = swipeRefreshState,
+                onRefresh =
+                { viewModel.onEvent(CompanyListingsEvent.Refresh) },
             ) {
-                items(state.companies.size) { i ->
-                    val company = state.companies[i]
-                    CompanyItem(
-                        company = company,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                navigator.navigate(
-                                    CompanyInfoScreenDestination(company.symbol)
-                                )
-                            }
-                            .padding(16.dp)
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(state.companies.size) { i ->
+                        val company = state.companies[i]
+                        CompanyItem(
+                            company = company,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    navigator.navigate(
+                                        CompanyInfoScreenDestination(company.symbol)
+                                    )
+                                }
+                                .padding(16.dp)
                         )
-                    if (i < state.companies.size){
-                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                        if (i < state.companies.size) {
+                            Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                        }
                     }
                 }
             }
